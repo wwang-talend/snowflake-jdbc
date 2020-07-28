78e72c076e9f42cab45a4c5ffaa538925613fa84
sfc-gh-mknister <meg.knister@snowflake.com>
2020-07-28 23:41:52 +0000

    parsed through GS command for threshold

--- src/main/java/net/snowflake/client/jdbc/SnowflakeFileTransferAgent.java
+++ src/main/java/net/snowflake/client/jdbc/SnowflakeFileTransferAgent.java
@@ -4,8 +4,6 @@
 
 package net.snowflake.client.jdbc;
 
-import static net.snowflake.client.jdbc.SnowflakeUtil.systemGetProperty;
-
 import com.amazonaws.util.Base64;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -14,19 +12,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Strings;
 import com.google.common.io.ByteStreams;
 import com.google.common.io.CountingOutputStream;
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.security.DigestOutputStream;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.sql.SQLException;
-import java.util.*;
-import java.util.Map.Entry;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.zip.GZIPOutputStream;
 import net.snowflake.client.core.*;
 import net.snowflake.client.jdbc.cloud.storage.*;
 import net.snowflake.client.jdbc.telemetryOOB.TelemetryService;
@@ -42,6 +27,22 @@ import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.filefilter.WildcardFileFilter;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.DigestOutputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.zip.GZIPOutputStream;
+
+import static net.snowflake.client.jdbc.SnowflakeUtil.systemGetProperty;
 
 /**
  * Class for uploading/downloading files
@@ -79,7 +80,8 @@ public class SnowflakeFileTransferAgent implements SnowflakeFixedView {
   // with 4 threads by default
   private Set<String> smallSourceFiles;
 
-  private static final int BIG_FILE_THRESHOLD = 64 * 1024 * 1024;
+  /* Default value, in bytes (200 mb) */
+  private static int BIG_FILE_THRESHOLD = 200000000;
 
   private Map<String, FileMetadata> fileMetadataMap;
 
@@ -873,6 +875,14 @@ public class SnowflakeFileTransferAgent implements SnowflakeFixedView {
           SqlState.INTERNAL_ERROR,
           ErrorCode.INTERNAL_ERROR.getMessageCode(),
           "Failed to parse the locations due to: " + ex.getMessage());
+    }
+
+    JsonNode thresholdNode = jsonNode.path("data").path("threshold");
+    if (thresholdNode != null)
+    {
+      int threshold = thresholdNode.asInt();
+      // threshold put in megabytes, so multiply value by 1*10^6 to get byte value
+      BIG_FILE_THRESHOLD = threshold * 1000000;
     }
 
     showEncryptionParameter =
